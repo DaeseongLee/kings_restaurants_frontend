12ceca96ad010a425c387ae3d74183d35fe2f9ce
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-05-18 18:01:46 +0900

    Add build config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [], //난중에 배포할 때 필요한 css만 배포하도록 사용한 css 경로 지정해주는 곳
+  purge: ['./src/**/*.tsx'], //난중에 배포할 때 필요한 css만 배포하도록 사용한 css 경로 지정해주는 곳
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
